10346397eb91cb65d85d9fd863f811492916e48f
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2022-11-21 00:00:22 -0600

    Add mouseover event listener and changeColor function to script.js

--- script.js
+++ script.js
@@ -5,14 +5,25 @@ createGrid(16);
 function createGrid(size) {
     for(let i = 0; i < size; i++) {
         let col = document.createElement('div');
-        col.className = 'column';
+        col.classList.add('col');
         
         for(let j = 0; j < size; j++) {
             let row = document.createElement('div');
-            row.className = 'row';
+            row.classList.add('grid-square');
             row.textContent = `${j + 1}`;
             col.appendChild(row);
         }
         container.appendChild(col);
     }
 }
+
+const  gridSquare = document.querySelectorAll(".grid-square");
+
+gridSquare.forEach((square) => {
+    square.addEventListener("mouseover", changeColor);
+});
+
+function changeColor(e) {
+    e.target.classList.add('color');
+}
+
