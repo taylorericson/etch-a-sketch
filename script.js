61b2220f5f39686fbaf6c666b96adb2280b72c82
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2022-11-22 00:09:15 -0600

    Update gui style

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ const clearBtn = document.getElementById('clearBtn');
 sizeBtn.addEventListener('click', getGridSize);
 clearBtn.addEventListener('click', clearGrid);
 
+createGrid(16);
+
 function getGridSize() {
-    let gridSize = prompt("Enter size: ");
-    createGrid(gridSize);
+    let gridSize = parseInt(prompt("Enter size: "));
 
-    let gridSquare = document.querySelectorAll(".grid-square");
-
-    gridSquare.forEach((square) => {
-        square.addEventListener("mouseover", changeColor);
-    });
+    if(gridSize >= 1 && gridSize <= 100) {
+        createGrid(gridSize);
+    } else {
+        alert("Enter a positive number (1-100)");
+        getGridSize();
+    }
 }
 
 function createGrid(size) {
-    container.innerHTML = "";
+    container.textContent = "";
 
     for(let i = 0; i < size; i++) {
         let col = document.createElement('div');
@@ -26,11 +28,20 @@ function createGrid(size) {
         for(let j = 0; j < size; j++) {
             let row = document.createElement('div');
             row.classList.add('grid-square');
+            row.classList.add('row');
+            console.log(row.getAttribute('width'));
             row.textContent = `${j + 1}`;
             col.appendChild(row);
         }
         container.appendChild(col);
     }
+
+    let gridSquare = document.querySelectorAll(".grid-square");
+
+    gridSquare.forEach((square) => {
+        square.addEventListener("mouseover", changeColor);
+    });
+    
 }
 
 function changeColor(e) {
